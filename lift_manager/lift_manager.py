4f9419645cee0a8f7e0847ac454c8e879690ee7a
shamanyu <shubhamshamanyu@gmail.com>
2018-05-18 10:53:47 +0530

    Add Python boiler plate code to start building singleton lift_manager class

---
+++ lift_manager/lift_manager.py
@@ -0,0 +1,22 @@
+class lift_manager(object):
+
+  def lift_manager(self):
+    pass
+
+
+from nose.tools import assert_equals, assert_raises
+
+class test_lift_manager(object):
+
+  def test_lift_manager(self):
+    lift_manager = lift_manager()
+
+    print ("All test cases passed!")
+
+
+def main():
+    test_lift_manager = test_lift_manager()
+    test_lift_manager.test_lift_manager()
+
+if __name__ == '__main__':
+    main()
